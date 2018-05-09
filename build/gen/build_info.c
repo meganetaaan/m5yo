07502654967bef7169ba03ab68d09fe3831bcc78
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2018-05-09 09:07:00 +0900

    Removed files to ignore

--- build/gen/build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *build_id = "20180507-151624/???";
-const char *build_timestamp = "2018-05-07T15:16:24Z";
-const char *build_version = "0.1";
--- build/gen/mg_build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *mg_build_id = "20180507-151621/???";
-const char *mg_build_timestamp = "2018-05-07T15:16:21Z";
-const char *mg_build_version = "2018050715";
